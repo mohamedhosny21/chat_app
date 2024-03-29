6ea1eb849bb6a1eab44d85caf825564702451ea1
mohamedhosny21 <96444914+mohamedhosny21@users.noreply.github.com>
2024-03-29 03:34:01 +0200

    Add routes for login, home, OTP, contacts, and user chat screens
    Create a new file `routes.dart` in the `lib/core/app_router` directory

---
+++ lib/core/app_router/routes.dart
@@ -0,0 +1,7 @@
+class Routes {
+  static const String loginScreen = '/';
+  static const String homeScreen = '/home_screen';
+  static const String otpScreen = '/otp_screen';
+  static const String contactsScreen = '/contacts_screen';
+  static const String userChatScreen = '/user_chat_screen';
+}
