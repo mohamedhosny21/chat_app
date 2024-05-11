c9cb1397f603d1831a746603b4e9a9a04a715a29
mohamedhosny21 <96444914+mohamedhosny21@users.noreply.github.com>
2024-05-11 03:02:13 +0300

    Refactor main.dart for better code organization
    Move authentication logic to separate class and improve dependency injection.

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,47 @@
-import 'app_router/app_router.dart';
-import 'app_router/routes.dart';
+import 'package:chatify/core/dependency_injection/dependency_injection.dart';
+import 'package:chatify/features/login/logic/authentication_cubit/authentication_cubit.dart';
+
+import 'core/app_router/app_router.dart';
 import 'firebase_options.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-late String initialRoute;
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  if (FirebaseAuth.instance.currentUser == null) {
-    initialRoute = Routes.loginScreen;
-  } else {
-    initialRoute = Routes.homeScreen;
-  }
+  setupGetIt();
+  final initialRoute = await getIt<AuthenticationCubit>().getInitialRoute();
+
   runApp(ChatApp(
+    initialRoute: initialRoute,
     appRouter: AppRouter(),
   ));
 }
 
 class ChatApp extends StatelessWidget {
+  final String initialRoute;
   final AppRouter appRouter;
-  const ChatApp({super.key, required this.appRouter});
+  const ChatApp(
+      {super.key, required this.appRouter, required this.initialRoute});
 
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      designSize: const Size(375, 812),
-      minTextAdapt: true,
-      splitScreenMode: true,
-      child: MaterialApp(
-        title: 'Chatify',
-        initialRoute: initialRoute,
-        onGenerateRoute: appRouter.onGenerateRoute,
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-            textTheme: GoogleFonts.montserratTextTheme(),
-            scaffoldBackgroundColor: Colors.white),
-      ),
-    );
+        designSize: const Size(375, 812),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        child: MaterialApp(
+          title: 'Chatify',
+          initialRoute: initialRoute,
+          onGenerateRoute: appRouter.onGenerateRoute,
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+              textTheme: GoogleFonts.montserratTextTheme(),
+              scaffoldBackgroundColor: Colors.white),
+        ));
   }
 }
