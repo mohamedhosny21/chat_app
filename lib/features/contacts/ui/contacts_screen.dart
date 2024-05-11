8d39eea961f5a07ccdc651665d4b8c890d452a99
mohamedhosny21 <96444914+mohamedhosny21@users.noreply.github.com>
2024-05-11 03:46:20 +0300

    finish the UI of contacts screen and create the contact item

--- lib/features/contacts/ui/contacts_screen.dart
+++ lib/features/contacts/ui/contacts_screen.dart
@@ -1,13 +1,12 @@
-import 'package:chatify/constants/colors.dart';
-import 'package:chatify/constants/dimensions.dart';
-import 'package:chatify/constants/styles.dart';
-import 'package:chatify/widgets/circular_progress_indicator.dart';
-import 'package:chatify/widgets/snackbar.dart';
+import 'package:chatify/core/helpers/dimensions.dart';
+import 'package:chatify/core/theming/styles.dart';
+import 'package:chatify/core/helpers/snackbar.dart';
+import 'package:chatify/features/contacts/data/contact_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-import '../logic/contacts_cubit/cubit/contacts_cubit.dart';
+import '../logic/cubit/contacts_cubit.dart';
 import 'widgets/contacts_item_widget.dart';
 
 class ContactsScreen extends StatefulWidget {
@@ -18,34 +17,47 @@ class ContactsScreen extends StatefulWidget {
 }
 
 class _ContactsScreenState extends State<ContactsScreen> {
+  late ContactsCubit _contactsCubit;
   @override
   void initState() {
     super.initState();
+    _contactsCubit = context.read<ContactsCubit>();
+    _contactsCubit.showFilteredContacts();
+    _contactsCubit.listenChangedContacts();
+  }
 
-    context.read<ContactsCubit>().showFilteredContacts();
+  @override
+  void dispose() {
+    _contactsCubit.closeListeners();
+
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          leading: IconButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            icon: const Icon(
-              Icons.arrow_back_ios_new_sharp,
-            ),
-            iconSize: 24.w,
-          ),
-          backgroundColor: AppColors.lightPink,
+          backgroundColor: Colors.white,
+          leading: _appBarLeading(),
           title: Text(
             'My Contacts',
             style: AppStyles.font18Black600Weight,
           )),
       body: RefreshIndicator(
-          onRefresh: () => context.read<ContactsCubit>().showFilteredContacts(),
+          onRefresh: () => _contactsCubit.showFilteredContacts(),
           child: _buildContactsBlocConsumer()),
+    );
+  }
+
+  IconButton _appBarLeading() {
+    return IconButton(
+      onPressed: () {
+        Navigator.pop(context);
+      },
+      icon: const Icon(
+        Icons.arrow_back_ios_new_sharp,
+      ),
+      iconSize: 24.w,
     );
   }
 
@@ -54,42 +66,43 @@ class _ContactsScreenState extends State<ContactsScreen> {
       builder: (context, state) {
         if (state is ContactsLoadedState) {
           return Padding(
-            padding: AppDimensions.paddingTop12,
-            child: ListView.separated(
-              separatorBuilder: (context, index) =>
-                  AppDimensions.verticalSpacing16,
-              itemBuilder: (context, index) {
-                if (state.filteredContacts.isNotEmpty) {
-                  return ContactItem(
-                    contact: state.filteredContacts[index],
-                    contactId: state.filteredContactsId[index],
-                  );
-                } else {
-                  return Center(
-                    child: Text(
-                      'No Contacts !',
-                      style: AppStyles.font20GreyBold,
-                    ),
-                  );
-                }
-              },
-              itemCount: state.filteredContacts.length,
-            ),
-          );
-        } else {
-          return const SizedBox();
+              padding: AppDimensions.paddingTop12,
+              child: _buildContactsListView(state.filteredContacts));
         }
+        return const SizedBox();
       },
       listener: (context, state) {
-        if (state is ContactsLoadingState) {
-          showCircularProgressIndicator(context);
-        } else if (state is ContactsLoadedState) {
-          Navigator.pop(context);
-        } else if (state is ContactsErrorState) {
+        if (state is ContactsErrorState) {
           Navigator.pop(context);
           showErrorSnackBar(context, state.errorMsg);
         }
       },
+    );
+  }
+
+  Widget _buildContactsListView(List<ContactModel> filteredContacts) {
+    if (filteredContacts.isEmpty) {
+      return ListWheelScrollView(
+        physics: const AlwaysScrollableScrollPhysics(),
+        itemExtent: 30,
+        children: [
+          Center(
+            child: Text(
+              'No Contacts !',
+              style: AppStyles.font20GreyBold,
+            ),
+          ),
+        ],
+      );
+    }
+    return ListView.separated(
+      separatorBuilder: (context, index) => AppDimensions.verticalSpacing16,
+      itemBuilder: (context, index) {
+        return ContactItem(
+          contact: filteredContacts[index],
+        );
+      },
+      itemCount: filteredContacts.length,
     );
   }
 }
--- lib/features/contacts/ui/widgets/contacts_item_widget.dart
+++ lib/features/contacts/ui/widgets/contacts_item_widget.dart
@@ -1,46 +1,49 @@
-import 'package:chatify/constants/constants.dart';
-import 'package:chatify/constants/styles.dart';
+import 'package:chatify/core/helpers/constants.dart';
+import 'package:chatify/core/theming/styles.dart';
+import 'package:chatify/features/contacts/data/contact_model.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_contacts/contact.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-import '../../../../app_router/routes.dart';
+import '../../../../core/app_router/routes.dart';
 
 class ContactItem extends StatelessWidget {
-  final Contact contact;
-  final String contactId;
-  const ContactItem(
-      {super.key, required this.contact, required this.contactId});
+  final ContactModel contact;
+  const ContactItem({super.key, required this.contact});
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        Navigator.pushNamed(context, Routes.userChatScreen,
-            arguments: {"contact": contact, "contactId": contactId});
-        debugPrint(contactId);
+        Navigator.pushNamed(context, Routes.chatRoomScreen, arguments: contact);
+        debugPrint(contact.id.toString());
       },
       child: Column(
         children: [
           ListTile(
-            leading: CircleAvatar(
-              radius: 30.r,
-              child: contact.photo != null
-                  ? Image.asset(contact.photo.toString())
-                  : Image.asset(AppConstants.defaultUserPhoto),
-            ),
-            title: Text(
-              contact.displayName.isNotEmpty
-                  ? contact.displayName
-                  : contact.phones
-                      .map((devicePhoneNumber) =>
-                          devicePhoneNumber.normalizedNumber)
-                      .toString(),
-              style: AppStyles.font25BlackBold.copyWith(fontSize: 16.sp),
-            ),
+            leading: _contactPhoto(),
+            subtitle: contact.name.isNotEmpty
+                ? Text(contact.phoneNumber)
+                : const SizedBox(),
+            title: _contactName(),
           )
         ],
       ),
+    );
+  }
+
+  CircleAvatar _contactPhoto() {
+    return CircleAvatar(
+      radius: 30.r,
+      child: contact.profilePicture != null
+          ? Image.asset(contact.profilePicture.toString())
+          : Image.asset(AppConstants.defaultUserPhoto),
+    );
+  }
+
+  Text _contactName() {
+    return Text(
+      contact.name.isNotEmpty ? contact.name : contact.phoneNumber.toString(),
+      style: AppStyles.font25BlackBold.copyWith(fontSize: 16.sp),
     );
   }
 }
