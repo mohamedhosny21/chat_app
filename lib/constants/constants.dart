9f1ea18548af86fc93107512396835a243bb0c79
mohamedhosny21 <96444914+mohamedhosny21@users.noreply.github.com>
2024-05-11 03:15:58 +0300

    deleted files

--- lib/constants/constants.dart
+++
@@ -1,6 +0,0 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-
-class AppConstants {
-  static const String defaultUserPhoto = 'assets/images/default_user.png';
-  static final database = FirebaseFirestore.instance;
-}
