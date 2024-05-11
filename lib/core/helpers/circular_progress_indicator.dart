b7817df9317cbb33e605d4fc34e4aa3066103396
mohamedhosny21 <96444914+mohamedhosny21@users.noreply.github.com>
2024-05-11 03:14:18 +0300

    add circular progress indicator

---
+++ lib/core/helpers/circular_progress_indicator.dart
@@ -0,0 +1,19 @@
+import '../theming/colors.dart';
+import 'package:flutter/material.dart';
+
+void showCircularProgressIndicator(BuildContext context) {
+  AlertDialog alertDialog = const AlertDialog(
+    backgroundColor: Colors.transparent,
+    elevation: 0.0,
+    content: Center(
+      child: CircularProgressIndicator(
+        color: AppColors.mainBlack,
+      ),
+    ),
+  );
+  showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) => alertDialog,
+      barrierColor: Colors.grey.withOpacity(0.2));
+}
