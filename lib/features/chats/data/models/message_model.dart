1715169c2c584f8684f223d32827e6c2cc98a66d
mohamedhosny21 <96444914+mohamedhosny21@users.noreply.github.com>
2024-05-17 03:23:40 +0300

    change the data type of message time to timestamp

---
+++ lib/features/chats/data/models/message_model.dart
@@ -0,0 +1,47 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Message {
+  final String id;
+  final String senderId;
+  final String receiverId;
+  final String text;
+  final String type;
+  final Timestamp time;
+  final String status;
+  final bool isDeleted;
+
+  Message(
+      {required this.id,
+      required this.senderId,
+      required this.receiverId,
+      required this.text,
+      required this.type,
+      required this.time,
+      required this.status,
+      required this.isDeleted});
+
+  factory Message.fromMap(Map<String, dynamic> data) {
+    return Message(
+        id: data['id'],
+        senderId: data['senderId'],
+        receiverId: data['receiverId'],
+        text: data['text'],
+        type: data['type'],
+        time: data['time'],
+        status: data['status'],
+        isDeleted: data["isDeleted"]);
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      "id": id,
+      "senderId": senderId,
+      "receiverId": receiverId,
+      "text": text,
+      "type": type,
+      "time": time,
+      "status": status,
+      "isDeleted": isDeleted
+    };
+  }
+}
