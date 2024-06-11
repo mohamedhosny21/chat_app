7d5ba0f7b5cf630df70002a169392af99ab78ee1
mohamedhosny21 <96444914+mohamedhosny21@users.noreply.github.com>
2024-06-11 17:43:44 +0300

    add document message widget

---
+++ lib/features/chats/ui/widgets/chatroom_document_message.dart
@@ -0,0 +1,46 @@
+import 'package:chatify/features/chats/data/models/message_model.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../../core/theming/colors.dart';
+import '../../../../core/theming/styles.dart';
+import '../../logic/cubit/chat_cubit.dart';
+
+class ChatroomDocumentMessage extends StatelessWidget {
+  final Message message;
+  final bool isSentByMe;
+
+  const ChatroomDocumentMessage(
+      {super.key, required this.message, required this.isSentByMe});
+
+  @override
+  Widget build(BuildContext context) {
+    final String documentName =
+        context.read<ChatCubit>().extractFileNameFromUrl(message.text);
+    return GestureDetector(
+      onTap: () {
+        context.read<ChatCubit>().viewDocumentFile(message.text);
+      },
+      child: Column(
+        children: [
+          ListTile(
+            leading: Icon(
+              Icons.insert_drive_file,
+              color: isSentByMe ? Colors.white : AppColors.mainBlack,
+              size: 40.w,
+            ),
+            title: Text(
+              documentName,
+              style: isSentByMe
+                  ? AppStyles.font15White500Weight
+                      .copyWith(fontWeight: FontWeight.bold)
+                  : AppStyles.font15Black500Weight
+                      .copyWith(fontWeight: FontWeight.bold),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
