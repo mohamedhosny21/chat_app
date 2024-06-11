79bcb1316ee9b53ce480d25dd01aae4015c90144
mohamedhosny21 <96444914+mohamedhosny21@users.noreply.github.com>
2024-06-11 17:34:10 +0300

    add loading gifs for sender and receiver messages

--- lib/core/helpers/constants.dart
+++ lib/core/helpers/constants.dart
@@ -1,3 +1,7 @@
 class AppConstants {
   static const String defaultUserPhoto = 'assets/images/default_user.png';
+  static const String receiverLoadingGif =
+      'assets/gifs/receiver_loading_indicator.gif';
+  static const String senderLoadingGif =
+      'assets/gifs/sender_loading_indicator.gif';
 }
