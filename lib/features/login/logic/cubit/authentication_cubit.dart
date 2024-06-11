a4b5dc76f63bb87c0511d374a7f19e8f55f18e8a
mohamedhosny21 <96444914+mohamedhosny21@users.noreply.github.com>
2024-06-11 17:47:33 +0300

    add new folders for authentication

---
+++ lib/features/login/logic/cubit/authentication_cubit.dart
@@ -0,0 +1,130 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_contacts/flutter_contacts.dart';
+
+import '../../../../core/app_router/routes.dart';
+import '../../../../core/helpers/constants.dart';
+
+part 'authentication_state.dart';
+
+class AuthenticationCubit extends Cubit<AuthenticationState> {
+  final firestoreDatabase = FirebaseFirestore.instance;
+
+  final GlobalKey<FormState> phoneAuthFormKey = GlobalKey<FormState>();
+  final GlobalKey<FormState> pinFormKey = GlobalKey<FormState>();
+  AuthenticationCubit() : super(AuthenticationInitial());
+
+  Future<void> loginWithPhoneNumber(String phoneNumber) async {
+    emit(LoginLoadingState());
+
+    await FirebaseAuth.instance.verifyPhoneNumber(
+        phoneNumber: phoneNumber,
+        //time taken to recieve sms message
+        timeout: const Duration(seconds: 15),
+
+        //Automatic handling of the SMS code on Android devices
+        verificationCompleted: onVerificationCompleted,
+        //Handle failure events such as invalid phone numbers or whether the SMS quota has been exceeded.
+        verificationFailed: onVerificationFailed,
+
+        // Save the verification ID and show a UI to enter the code
+        // Handle when a code has been sent to the device from Firebase, used to prompt users to enter the code.
+        codeSent: onCodeSent,
+        //Handle a timeout of when automatic SMS code handling fails.
+        codeAutoRetrievalTimeout: onCodeAutoRetrievalTimeout);
+  }
+
+  void onVerificationCompleted(PhoneAuthCredential credential) async {
+    debugPrint('Auto Verification Completed');
+
+    await signIn(credential);
+  }
+
+  void onVerificationFailed(FirebaseAuthException error) {
+    debugPrint(error.toString());
+    emit(LoginFailedState(errorMsg: 'The number isn\'t valid'));
+  }
+
+  void onCodeSent(String verificationId, int? resendToken) async {
+    emit(PhoneNumberSubmittedState(verificationId: verificationId));
+  }
+
+  void onCodeAutoRetrievalTimeout(String verificationId) {
+    debugPrint('codeAutoRetrievalTimeout !');
+  }
+
+  Future<void> signIn(PhoneAuthCredential credential) async {
+    try {
+      await FirebaseAuth.instance.signInWithCredential(credential);
+      emit(LoginSuccessState(successMsg: 'Verification Completed'));
+    } catch (error) {
+      emit(LoginFailedState(errorMsg: 'Incorrect OTP code'));
+      throw Exception(error);
+    }
+  }
+
+  Future<void> signOut() async {
+    await FirebaseAuth.instance.signOut();
+  }
+
+  //function called when code is submitted and entered manually by user
+  Future<void> submitPhoneOtp(String smsCode, String verificationId) async {
+    emit(LoginLoadingState());
+    PhoneAuthCredential phoneAuthCredential = PhoneAuthProvider.credential(
+        verificationId: verificationId, smsCode: smsCode);
+    debugPrint('verificationId : $verificationId');
+    await signIn(phoneAuthCredential);
+  }
+
+  void createNewUser({required String phoneNumber}) async {
+    final newUser = <String, dynamic>{
+      "phone_number": phoneNumber,
+      "photo": AppConstants.defaultUserPhoto
+    };
+    FirebaseAuth.instance.authStateChanges().listen((currentUser) async {
+      debugPrint(currentUser?.uid);
+      await firestoreDatabase
+          .collection("Users")
+          .doc(currentUser!.uid)
+          .set(newUser);
+      emit(UserCreationState());
+    });
+  }
+
+  Future<bool> checkUserExists() async {
+    final currentUser = FirebaseAuth.instance.currentUser;
+
+    final existingUserQuery =
+        await firestoreDatabase.collection("Users").doc(currentUser?.uid).get();
+    return existingUserQuery.exists;
+  }
+
+  Future<String> getInitialRoute() async {
+    final currentUser = FirebaseAuth.instance.currentUser;
+
+    String initialRoute = Routes.loginScreen;
+
+    if (currentUser != null) {
+      bool userExists = await checkUserExists();
+      if (!userExists) {
+        await FirebaseAuth.instance.signOut();
+      } else {
+        initialRoute = Routes.homeScreen;
+      }
+    }
+    return initialRoute;
+  }
+
+  Future<bool> requestContactsPermission() async {
+    final requestPermission = await FlutterContacts.requestPermission();
+    if (requestPermission) {
+      emit(ContactsPermissionAcceptedState());
+      return requestPermission;
+    } else {
+      emit(ContactsPermissionDeniedState(errorMsg: 'Permission Denied !'));
+      throw Exception('Permission Denied !');
+    }
+  }
+}
---
+++ lib/features/login/logic/cubit/authentication_state.dart
@@ -0,0 +1,39 @@
+part of 'authentication_cubit.dart';
+
+sealed class AuthenticationState {}
+
+final class AuthenticationInitial extends AuthenticationState {}
+
+class LoginLoadingState extends AuthenticationState {}
+
+class PhoneNumberSubmittedState extends AuthenticationState {
+  final String verificationId;
+
+  PhoneNumberSubmittedState({required this.verificationId});
+}
+
+class LoginSuccessState extends AuthenticationState {
+  final String successMsg;
+
+  LoginSuccessState({required this.successMsg});
+}
+
+class LoginFailedState extends AuthenticationState {
+  final String errorMsg;
+
+  LoginFailedState({required this.errorMsg});
+}
+
+class AuthenticationUserState extends AuthenticationState {}
+
+class UnAuthenticationUserState extends AuthenticationState {}
+
+class UserCreationState extends AuthenticationState {}
+
+class ContactsPermissionAcceptedState extends AuthenticationState {}
+
+class ContactsPermissionDeniedState extends AuthenticationState {
+  final String errorMsg;
+
+  ContactsPermissionDeniedState({required this.errorMsg});
+}
