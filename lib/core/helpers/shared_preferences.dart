b01810a43e8baf293a5f7046036c03ecc2d59202
mohamedhosny21 <96444914+mohamedhosny21@users.noreply.github.com>
2024-06-11 17:35:07 +0300

    remove unnecessary shared prefs file

--- lib/core/helpers/shared_preferences.dart
+++
@@ -1,8 +0,0 @@
-import 'package:shared_preferences/shared_preferences.dart';
-
-class AppSharedPreferences {
-  static Future<String> getSavedLoggedUserId() async {
-    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
-    return sharedPreferences.getString('loggedUserId') ?? '';
-  }
-}
