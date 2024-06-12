205967e1a9d9ea16d5cb3e5f4b62519765558b3b
mohamedhosny21 <96444914+mohamedhosny21@users.noreply.github.com>
2024-06-12 20:03:42 +0300

    add bool to detecct is the message time widget is used in chatroom or ongoing chats screem

--- lib/features/chats/ui/widgets/message_time_widget.dart
+++ lib/features/chats/ui/widgets/message_time_widget.dart
@@ -3,31 +3,34 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-import '../../data/models/message_model.dart';
-
 class MessageTimeWidget extends StatelessWidget {
   final Timestamp messageTime;
   final TextStyle messageStyle;
+  final bool isInChatroom;
   const MessageTimeWidget({
     Key? key,
     required this.messageTime,
     required this.messageStyle,
+    required this.isInChatroom,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Text(
-      _showMostRecentMessageTime(messageTime),
+      _showMessageTime(messageTime, isInChatroom),
       style: messageStyle,
     );
   }
 
-  String _showMostRecentMessageTime(Timestamp messageTime) {
+  String _showMessageTime(Timestamp messageTime, bool isInChatroom) {
     final DateTime dateTime = messageTime.toDate().toLocal();
     final formattedDate = DateFormat('dd/MM/yyyy').format(dateTime);
     final now = DateFormat('dd/MM/yyyy').format(DateTime.now().toLocal());
     final yesterday = DateFormat('dd/MM/yyyy').format(
         DateTime.now().toLocal().subtract(const Duration(days: 1)).toLocal());
+    final localTime = messageTime.toDate().toLocal();
+    final formattedTime =
+        '${localTime.hour.toString().padLeft(2, '0')}:${localTime.minute.toString().padLeft(2, '0')}';
 
     // final DateTime messageDate =
     //     DateTime(dateTime.year, dateTime.month, dateTime.day);
@@ -37,14 +40,15 @@ class MessageTimeWidget extends StatelessWidget {
     //     DateTime(yesterday.year, yesterday.month, yesterday.day)
     //         .toLocal()
     //         .toString();
-
-    if (formattedDate == now) {
-      final localTime = messageTime.toDate().toLocal();
-      return '${localTime.hour.toString().padLeft(2, '0')}:${localTime.minute.toString().padLeft(2, '0')}';
-    } else if (formattedDate == yesterday) {
-      return 'Yesterday';
-    } else {
-      return formattedDate;
+    if (!isInChatroom) {
+      if (formattedDate == now) {
+        return formattedTime;
+      } else if (formattedDate == yesterday) {
+        return 'Yesterday';
+      } else {
+        return formattedDate;
+      }
     }
+    return formattedTime;
   }
 }
