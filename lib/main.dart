ca72452e0bea9a6314b54740430aa54fce448122
mohamedhosny21 <96444914+mohamedhosny21@users.noreply.github.com>
2024-06-27 03:57:27 +0300

    setup dotenv

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,25 @@
+import 'package:chatify/chatify_app.dart';
 import 'package:chatify/core/dependency_injection/dependency_injection.dart';
 import 'package:chatify/features/login/logic/cubit/authentication_cubit.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'core/app_router/app_router.dart';
 import 'firebase_options.dart';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  // await FirebaseAppCheck.instance
-  //     .activate(androidProvider: AndroidProvider.debug);
-  setupGetIt();
+
+  await setupGetIt();
   final initialRoute = await getIt<AuthenticationCubit>().getInitialRoute();
 
-  runApp(ChatApp(
+  runApp(ChatifyApp(
     initialRoute: initialRoute,
     appRouter: AppRouter(),
   ));
-}
-
-class ChatApp extends StatelessWidget {
-  final String initialRoute;
-  final AppRouter appRouter;
-  const ChatApp(
-      {super.key, required this.appRouter, required this.initialRoute});
-
-  @override
-  Widget build(BuildContext context) {
-    return ScreenUtilInit(
-        designSize: const Size(375, 812),
-        minTextAdapt: true,
-        splitScreenMode: true,
-        child: MaterialApp(
-          title: 'Chatify',
-          initialRoute: initialRoute,
-          onGenerateRoute: appRouter.onGenerateRoute,
-          debugShowCheckedModeBanner: false,
-          theme: ThemeData(
-              textTheme: GoogleFonts.montserratTextTheme(),
-              scaffoldBackgroundColor: Colors.white),
-        ));
-  }
 }
