e0d99da39e2ddb012dae4d48f2b0b624ac460aca
mohamedhosny21 <96444914+mohamedhosny21@users.noreply.github.com>
2024-06-27 03:58:54 +0300

    delete unnecessary file

--- lib/core/helpers/constants.dart
+++
@@ -1,7 +0,0 @@
-class AppConstants {
-  static const String defaultUserPhoto = 'assets/images/default_user.png';
-  static const String receiverLoadingGif =
-      'assets/gifs/receiver_loading_indicator.gif';
-  static const String senderLoadingGif =
-      'assets/gifs/sender_loading_indicator.gif';
-}
