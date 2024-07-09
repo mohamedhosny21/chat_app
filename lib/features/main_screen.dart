5f33c38e8eb292e20d089505f8e6605f0317265e
mohamedhosny21 <96444914+mohamedhosny21@users.noreply.github.com>
2024-07-10 00:57:10 +0300

    create cubit on profile screen

--- lib/features/main_screen.dart
+++ lib/features/main_screen.dart
@@ -1,11 +1,11 @@
 import 'package:chatify/core/dependency_injection/dependency_injection.dart';
 import 'package:chatify/features/chats/logic/cubit/chat_cubit.dart';
-import 'package:chatify/features/groups/ui/groups_screen.dart';
+import 'package:chatify/features/profile/logic/cubit/profile_cubit.dart';
 
 import 'calls/calls_screen.dart';
 import 'chats/ui/screens/chats_screen.dart';
 import 'login/logic/cubit/authentication_cubit.dart';
-import 'profile/profile_screen.dart';
+import 'profile/ui/profile_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -29,9 +29,11 @@ class _NavBarPagesState extends State<NavBarPages> {
       ],
       child: const ChatsScreen(),
     ),
-    const GroupsScreen(),
     const CallsScreen(),
-    const ProfileScreen()
+    BlocProvider<ProfileCubit>(
+      create: (context) => getIt<ProfileCubit>(),
+      child: const ProfileScreen(),
+    )
   ];
   int index = 0;
 
@@ -47,15 +49,10 @@ class _NavBarPagesState extends State<NavBarPages> {
             });
           },
           selectedItemColor: Colors.black,
-          type: BottomNavigationBarType.fixed,
           items: const [
             BottomNavigationBarItem(
               icon: Icon(Icons.messenger_sharp),
               label: 'Chats',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.groups),
-              label: 'Groups',
             ),
             BottomNavigationBarItem(
               icon: Icon(Icons.call),
