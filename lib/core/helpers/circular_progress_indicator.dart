dad1895dca09994e8bddbb0ad4a6b8a307abd621
mohamedhosny21 <96444914+mohamedhosny21@users.noreply.github.com>
2024-07-10 00:51:44 +0300

    add color parameter

--- lib/core/helpers/circular_progress_indicator.dart
+++ lib/core/helpers/circular_progress_indicator.dart
@@ -1,13 +1,13 @@
 import '../theming/colors.dart';
 import 'package:flutter/material.dart';
 
-void showCircularProgressIndicator(BuildContext context) {
-  AlertDialog alertDialog = const AlertDialog(
+void showCircularProgressIndicator(BuildContext context, {Color? color}) {
+  AlertDialog alertDialog = AlertDialog(
     backgroundColor: Colors.transparent,
     elevation: 0.0,
     content: Center(
       child: CircularProgressIndicator(
-        color: AppColors.mainBlack,
+        color: color ?? AppColors.mainBlack,
       ),
     ),
   );
