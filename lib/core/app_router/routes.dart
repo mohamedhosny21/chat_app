dcef3be8c156c596861d42ca5ac8b82b914bc4c7
mohamedhosny21 <96444914+mohamedhosny21@users.noreply.github.com>
2024-08-12 23:49:07 +0300

    add new routes

--- lib/core/app_router/routes.dart
+++ lib/core/app_router/routes.dart
@@ -1,9 +1,14 @@
 class Routes {
   static const String loginScreen = '/login_screen';
   static const String homeScreen = '/home_screen';
+  static const String callsScreen = '/calls_screen';
   static const String otpScreen = '/otp_screen';
   static const String contactsScreen = '/contacts_screen';
   static const String chatRoomScreen = '/chat_room_screen';
   static const String videoMessageScreen = '/video_message_screen';
   static const String imageMessageScreen = '/image_message_screen';
+  static const String textStoryPreviewScreen = '/text_story_preview_screen';
+  static const String imageStoryPreviewScreen = '/image_story_preview_screen';
+  static const String videoStoryPreviewScreen = '/video_story_preview_screen';
+  static const String storyViewScreen = '/story_view_screen';
 }
